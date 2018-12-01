74353943c8fb2008decb954fc91ecdeb5103c16c
Arthur York <ahyork98@gmail.com>
2018-11-30 21:57:30 -0800

    sending messages should store in database, successfully loads from database when opening

--- public/index.js
+++ public/index.js
@@ -4,9 +4,22 @@ var sendButton = document.getElementById("send-button");
 sendButton.addEventListener('click', function(event) {
   var textBoxContent = textBox.value;
   if (textBoxContent !== "") {
-    console.log("sent message");
-    sendMessage(textBoxContent);
-    textBox.value = "";
+    var postRequest = new XMLHttpRequest();
+    var requestURL = '/message/addMessage';
+    postRequest.open('POST', requestURL);
+
+    var reuqestBody = JSON.stringify({
+      message: textBoxContent
+    });
+
+    postRequest.addEventListener('load', function (even) {
+      if (event.target.status === 200) {
+        console.log("sent message");
+        sendMessage(textBoxContent);
+        textBox.value = "";
+      }
+    });
+
   }
 });
 
