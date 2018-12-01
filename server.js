7318c31abb777cf64c1bd1bd7b639d2c4559ffba
Arthur York <ahyork98@gmail.com>
2018-11-30 20:03:31 -0800

    fixed untracked files, added mongo skeleton to server.js, set body margin to 0 in style.css

--- server.js
+++ server.js
@@ -1,11 +1,27 @@
 var path = require('path');
 var express = require('express');
+
+var MongoClient = require('mongodb').MongoClient;
+
+var mongoHost = process.env.MONGO_HOST;
+var mongoPort = process.env.MONGO_PORT || '27017';
+var mongoUsername = process.env.MONGO_USERNAME;
+var mongoPassword = process.env.MONGO_PASSWORD;
+var mongoDBName = process.env.MONGO_DB_NAME;
+
+var mongoURL = "mongodb://" + mongoUsername + ":" + mongoPassword + "@" + mongoHost +
+    ":" + mongoPort + "/" + mongoDBName;
+
 var app = express();
 var exphbs = require('express-handlebars');
+
+var port = process.env.PORT || 3000;
 
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
+
+//==Handle incoming requests
 app.use(express.static('public'));
 
 app.get('/', function (req, res, next) {
@@ -17,20 +33,18 @@ app.get('*', function (req, res) {
   res.status(404).render('404', {});
 });
 
-//Set the server to listen on the appropriate PORT
-if (process.env.PORT) {
-  app.listen(process.env.PORT, function(err) {
+//Set up MongoDB Connection
+MongoClient.connect(mongoURL, function(err, client) {
+  if (err) {
+    throw err;
+  }
+
+  //Set the server to listen on the appropriate PORT
+  app.listen(port, function(err) {
     if (err) {
       throw err;
     }
-    console.log("==Server listening on port", process.env.PORT);
+    console.log("==Server listening on port", port);
   });
-}
-else {
-  app.listen(3000, function(err) {
-    if (err) {
-      throw err;
-    }
-    console.log("==Server listening on port 3000");
-  });
-}
+});
+
