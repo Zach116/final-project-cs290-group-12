e7b3861f81fa96527e865b9e16151c10c2903a7e
seikamoomoo <seika.mahmud@gmail.com>
2018-12-04 00:10:45 -0800

    color changes

--- public/messageTemplate.js
+++ public/messageTemplate.js
@@ -3,9 +3,9 @@
 templates['messageBubble'] = template({"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
     var helper, alias1=depth0 != null ? depth0 : (container.nullContext || {}), alias2=helpers.helperMissing, alias3="function", alias4=container.escapeExpression;
 
-  return "<div class=\"messageBubble\">\n  <p>\n    <div id=\"text\">\n      "
+  return "<div class=\"messageBubble\">\n  <p>\n    <div id=\"username-text\">\n      "
     + alias4(((helper = (helper = helpers.username || (depth0 != null ? depth0.username : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"username","hash":{},"data":data}) : helper)))
-    + "\n    </div>\n    <div id=\"text\">\n      "
+    + "\n    </div>\n    <div id=\"message-text\">\n      "
     + alias4(((helper = (helper = helpers.message || (depth0 != null ? depth0.message : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"message","hash":{},"data":data}) : helper)))
     + "\n    </div>\n  </p>\n</div>\n";
 },"useData":true});
